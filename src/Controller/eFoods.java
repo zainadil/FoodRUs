9f33a55abf4284ca5b8cbcda45ce8081f2f18046
Zain Adil <xain90@gmail.com>
2013-11-28 16:28:26 -0500

    URL Changes

--- src/Controller/eFoods.java
+++ src/Controller/eFoods.java
@@ -16,7 +16,7 @@ import model.*;
 /**
  * Servlet implementation class Start
  */
-@WebServlet("/eFoods")
+@WebServlet("/eFoods/*")
 public class eFoods extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
