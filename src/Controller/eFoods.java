d6a5220126c27a71354229755cc691c3b011c6f2
saadrizvi <ibrizvi@gmail.com>
2013-11-29 04:10:34 -0500

    Big Change
    Marshalling + changing some logic around + documentation.

--- src/Controller/eFoods.java
+++ src/Controller/eFoods.java
@@ -44,12 +44,22 @@ public class eFoods extends HttpServlet {
 			RequestDispatcher rd;
 			HttpSession session = request.getSession();
 			String pageURI = request.getRequestURI();
-			request.setAttribute("loggedIn", session.getAttribute("loggedIn"));
+
+			// check to see if the basket for the session exists, and then load
+			// it into the request. (We use this in JSPX)
 			if (session.getAttribute("basket") == null) {
 				HashMap<String, Integer> basket = new HashMap<String, Integer>();
 				session.setAttribute("basket", basket);
 			}
+			// check to see if the client is logged in or not, load it into the
+			// request. (We use this in JSPX)
+			if (session.getAttribute("client") != null) {
+				request.setAttribute("client", session.getAttribute("client"));
+				request.setAttribute("loggedIn", session.getAttribute("loggedIn"));
+			}
 
+			// Determine what the Controller should do and where it should be
+			// passed to.
 			if (pageURI.contains("Category")) {
 				category(pageURI, model, request, response);
 			} else if (pageURI.contains("Login")) {
@@ -58,7 +68,9 @@ public class eFoods extends HttpServlet {
 				logout(pageURI, model, request, response);
 			} else if (pageURI.contains("Cart")) {
 				cart(pageURI, model, request, response);
-			} else {
+			} else if (pageURI.contains("Checkout")) {
+				checkout(pageURI, model, request, response);
+			} else { // Always fall back to the homepage
 				rd = getServletContext().getRequestDispatcher("/views/homePage.jspx");
 				rd.forward(request, response);
 			}
@@ -77,21 +89,56 @@ public class eFoods extends HttpServlet {
 		this.doGet(request, response);
 	}
 
+	/***
+	 * The client has chosen to logout, check to see if the client is even
+	 * logged in (which it should be) and log out. End session.
+	 * 
+	 * @param pageURI
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 * 
+	 *             TO-DO: Business decision to be made: Should logging out get
+	 *             rid of basket or not? Would logging out, save the basket to a
+	 *             cookie? and Logging in, load the basket from cookie if it
+	 *             exists?
+	 */
 	private void logout(String pageURI, FoodRus model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
-
-		System.out.println("Logging Out...");
+		session.removeAttribute("client");
+		System.out.println("Logging Out by..");
 		if (request.getAttribute("loggedIn") != null) session.removeAttribute("loggedIn");
 		response.sendRedirect(this.getServletContext().getContextPath() + "/eFoods");
 	}
 
+	/**
+	 * The client has chosen to login. Forward them to the loginPage and see
+	 * what they do. (Store the page they came from) If they choose to click on
+	 * the loginButton, then we should retrieve the fields from the form. We
+	 * should check credentials, if valid it should return to you a
+	 * "ClientBean". Redirect to the page they came from. else, if invalid
+	 * credentials, keep them on the loginPage and display an error.
+	 * 
+	 * Note: In ClientBean we do not want to store the password of the user.
+	 * 
+	 * @param pageURI
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @throws ServletException
+	 * @throws IOException
+	 * @throws SQLException
+	 * 
+	 * 
+	 */
 	private void login(String uri, FoodRus model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException,
 			SQLException {
 		RequestDispatcher rd;
 		HttpSession session = request.getSession();
 		ClientBean tmp;
 		if (request.getParameter("loginButton") != null) {
-			System.out.println("Came in here");
 			boolean loggedIn;
 			String accountNumber = request.getParameter("accountNumber");
 			request.setAttribute("accountNumber", accountNumber);
@@ -102,6 +149,7 @@ public class eFoods extends HttpServlet {
 				request.setAttribute("loggedIn", loggedIn);
 				session.setAttribute("loggedIn", true);
 				session.setAttribute("client", tmp);
+				if (session.getAttribute("returnTo") == null) response.sendRedirect(this.getServletContext().getContextPath() + "/eFoods");
 				response.sendRedirect((String) session.getAttribute("returnTo"));
 			} else {
 				loggedIn = false;
@@ -116,8 +164,24 @@ public class eFoods extends HttpServlet {
 		}
 	}
 
-	private void category(String uri, FoodRus model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException,
-			SQLException {
+	/**
+	 * Client has gone to the category page, here we will forward them
+	 * automatically to the itemPage.jspx which displays all items of a specific
+	 * category. Once the client picks which item they want to purchase or look
+	 * at, they will be sent to an item.jspx, where we will wait for them to
+	 * addToBasket. Once they add to Basket, we create a HashMap<ItemName,
+	 * Quantity>, and update the quantity as necessary.
+	 * 
+	 * Again we want to keep track of where they came from, and then redirect
+	 * them to it once they've added to basket.
+	 * 
+	 * @param uri
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 */
+	private void category(String uri, FoodRus model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		HttpSession session = request.getSession();
 		RequestDispatcher rd;
@@ -127,13 +191,16 @@ public class eFoods extends HttpServlet {
 			if (request.getParameter("addToBasket") != null) {
 				String itemID = request.getParameter("itemID");
 				int ItemQuantity = Integer.parseInt(request.getParameter("quantity"));
+				if (session.getAttribute("basket") == null) throw new Exception();
 				HashMap<String, Integer> basket = (HashMap<String, Integer>) session.getAttribute("basket");
 				if (basket.containsKey(itemID)) {
-					System.out.println("2");
 					basket.put(itemID, basket.get(itemID) + ItemQuantity);
 				} else basket.put(itemID, ItemQuantity);
 				session.setAttribute("basket", basket);
+
+				if (session.getAttribute("returnTo") == null) response.sendRedirect(this.getServletContext().getContextPath() + "/eFoods");
 				response.sendRedirect((String) session.getAttribute("returnTo"));
+
 			} else {
 				session.setAttribute("returnTo", request.getHeader("referer"));
 				String itemID = getItemID(uri);
@@ -143,28 +210,85 @@ public class eFoods extends HttpServlet {
 				rd.forward(request, response);
 			}
 		} else {
+			// Do we need this?
 			List<CategoryBean> catBean = model.retrieveCategories();
 			request.setAttribute("catBean", catBean);
 			List<ItemBean> itemList = model.retrieveItems(getCategory(uri));
 			request.setAttribute("itemList", itemList);
-
-			// Check if userLogged in
-			if (session.getAttribute("loggedIn") != null) request.setAttribute("loggedIn", true);
 
 			rd = getServletContext().getRequestDispatcher("/views/itemPage.jspx");
 			rd.forward(request, response);
 		}
 	}
 
-	private void cart(String uri, FoodRus model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException,
-			SQLException {
+	/**
+	 * Client has chosen to go to the Cart Page (Basket Page). Here the HashMap
+	 * from above is displayed, if null then it is shown as empty.
+	 * 
+	 * Client has the option to continue shopping or to proceed to checkout.
+	 * 
+	 * The page will also allow modification/updating their cart prior to
+	 * purchasing.
+	 * 
+	 * @param uri
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 * 
+	 */
+	private void cart(String uri, FoodRus model, HttpServletRequest request, HttpServletResponse response) throws Exception {
 		HttpSession session = request.getSession();
-		HashMap<String, Integer> basket = (HashMap<String, Integer>) session.getAttribute("basket");
-		CartBean cartBean = model.generateShopppingCart(basket, (ClientBean) request.getSession().getAttribute("client"));
+
+		CartBean cartBean = model.generateShopppingCart((HashMap<String, Integer>) session.getAttribute("basket"),
+				(ClientBean) session.getAttribute("client"));
+
 		request.setAttribute("sCart", cartBean);
 		RequestDispatcher rd;
+
 		rd = getServletContext().getRequestDispatcher("/views/cartPage.jspx");
 		rd.forward(request, response);
+	}
+
+	/***
+	 * The client should be sent to a confirmation page of their purchase order,
+	 * hence the checkout page. This should also generate the XML necessary for
+	 * the B2B aspect.
+	 * 
+	 * @param uri
+	 * @param model
+	 * @param request
+	 * @param response
+	 * @throws Exception
+	 * 
+	 *             To-Do: Add checkoutPage.jspx and redirect to it. Add
+	 *             implementation to Submit P/O which will create the XML
+	 *             necessary.
+	 * 
+	 *             Model.export should NOT be called without a CLIENT otherwise
+	 *             major issue.
+	 */
+	private void checkout(String uri, FoodRus model, HttpServletRequest request, HttpServletResponse response) throws Exception {
+		HttpSession session = request.getSession();
+		RequestDispatcher rd;
+		if (session.getAttribute("client") == null) {
+			System.out.println("Please Log in before checking out");
+
+		} else {
+			CartBean cartBean = model.generateShopppingCart((HashMap<String, Integer>) session.getAttribute("basket"),
+					(ClientBean) session.getAttribute("client"));
+			String filename = "D:\\test.xml";
+
+			model.export(cartBean, filename);
+
+			// 
+			// rd =
+			// getServletContext().getRequestDispatcher("/views/cartPage.jspx");
+			// rd.forward(request, response);
+
+			response.sendRedirect(this.getServletContext().getContextPath() + "/eFoods");
+		}
+
 	}
 
 	private String getItemID(String uri) {
--- src/model/FoodRus.java
+++ src/model/FoodRus.java
@@ -1,9 +1,32 @@
 package model;
 
+import java.io.FileWriter;
+import java.io.StringWriter;
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
+import java.util.TimeZone;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Marshaller;
+import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.Duration;
+import javax.xml.datatype.XMLGregorianCalendar;
+import javax.xml.namespace.QName;
+import javax.xml.transform.stream.StreamResult;
+
+import Marshalling.CustomerType;
+import Marshalling.ItemType;
+import Marshalling.ItemsType;
+import Marshalling.OrderType;
 
 public class FoodRus {
 	CategoryDAO categoryData;
@@ -52,18 +75,15 @@ public class FoodRus {
 
 	/***
 	 * Function for Shopping Cart
-	 * 
-	 * @throws SQLException
+	 * @throws Exception 
 	 ***/
-	public CartBean generateShopppingCart(HashMap<String, Integer> basket, ClientBean client) throws SQLException {
+	public CartBean generateShopppingCart(HashMap<String, Integer> basket, ClientBean client) throws Exception {
 		double hst = 13; // this is wrong. change it.
 		CartBean tmp = new CartBean();
 		ItemBean item = new ItemBean();
 		List<ItemBean> listItem = new LinkedList<ItemBean>();
 		double total = 0;
-		if (basket == null) {
-			return null;
-		}
+		
 		for (String key : basket.keySet()) {
 			basket.get(key);
 			item = itemData.retrieveItem(key);
@@ -76,7 +96,68 @@ public class FoodRus {
 		tmp.setItems(listItem);
 		tmp.setTotal(total);
 		tmp.setHST(hst);
-		tmp.setGrandTotal(total * ((hst/100) + 1));
+		tmp.setGrandTotal(total * ((hst / 100) + 1));
 		return tmp;
+	}
+
+	public void export(CartBean cart, String filename) throws Exception {
+		int x = 120;
+		Date now = new Date();
+
+		OrderType lw = createOrderType(x, now, cart);
+		JAXBContext jc = JAXBContext.newInstance(lw.getClass());
+		Marshaller marshaller = jc.createMarshaller();
+		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
+		marshaller.setProperty(Marshaller.JAXB_FRAGMENT, Boolean.TRUE);
+
+		StringWriter sw = new StringWriter();
+		sw.write("\n");
+		marshaller.marshal(lw, new StreamResult(sw));
+		System.out.println("wah");
+		System.out.println(sw.toString()); // for debugging
+		FileWriter fw = new FileWriter(filename);
+		fw.write(sw.toString());
+		fw.close();
+	}
+
+	private OrderType createOrderType(int x, Date now, CartBean cart) throws Exception {
+		OrderType lw = new OrderType();
+		// attributes of <order>
+		lw.setId(new BigInteger("" + x));
+
+		GregorianCalendar c = new GregorianCalendar();
+		c.setTime(now);
+		XMLGregorianCalendar date = DatatypeFactory.newInstance().newXMLGregorianCalendar(c);
+		lw.setSubmitted(date);
+
+		// customer information
+		CustomerType customer = new CustomerType();
+		customer.setAccount(cart.getCustomer().getNumber() + "");
+		customer.setName(cart.getCustomer().getName());
+		lw.setCustomer(customer);
+
+		// items
+		ItemsType items = new ItemsType();
+		List<ItemType> listItems = new ArrayList<ItemType>();
+		for (ItemBean tmp : cart.getItems()) {
+			ItemType item = new ItemType();
+
+			item.setNumber(tmp.getNumber());
+			item.setName(tmp.getName());
+			item.setPrice(new BigDecimal("" + tmp.getPrice()));
+			item.setQuantity(new BigInteger("" + tmp.getQty()));
+			item.setExtended(new BigDecimal("" + (tmp.getPrice() * tmp.getQty())));
+
+			listItems.add(item);
+		}
+		items.setItem(listItems);
+		lw.setItems(items);
+
+		// remaining elements
+		lw.setTotal(new BigDecimal("" + cart.getTotal()));
+		lw.setShipping(new BigDecimal("" + cart.getShipping()));
+		lw.setHST(new BigDecimal("" + cart.getHST()));
+		lw.setGrandTotal(new BigDecimal("" + cart.getGrandTotal()));
+		return lw;
 	}
 }// end business Logic
