d3cc0eefb4517ff29f7e4ca27404b7735077d36d
Zain Adil <xain90@gmail.com>
2013-11-29 21:27:55 -0500

    CSS Changes

--- WebContent/res/foodrus.js
+++ WebContent/res/foodrus.js
@@ -2,4 +2,5 @@
 function updateQuanity(itemID){
 	var qty = document.getElementById(itemID).value;
 	document.getElementById("updatedIDandQty").value = itemID + ";" + qty;
+	alert(document.getElementById("updatedIDandQty"));
 }
