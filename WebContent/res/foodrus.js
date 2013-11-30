84dff9b493e5c3dd4c1af505b80c4cd3a052dcec
Zain Adil <xain90@gmail.com>
2013-11-29 23:51:48 -0500

    Final Merge

--- WebContent/res/foodrus.js
+++ WebContent/res/foodrus.js
@@ -2,13 +2,9 @@
 function updateQuanity(itemID){
 	var qty = document.getElementById(itemID).value;
 	document.getElementById("updatedIDandQty").value = itemID + ";" + qty;
-<<<<<<< HEAD
 }
 
 function addtoCart(itemID){
 	var qty = document.getElementById(itemID).value;
 	document.getElementById("addedIDandQty").value = itemID + ";" + qty;
-=======
-//	alert(document.getElementById("updatedIDandQty"));
->>>>>>> 527b8ad293555609d5fe7115fd9c57ffd6ab0d3a
 }
