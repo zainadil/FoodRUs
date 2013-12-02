c8934baf9250d23de8ab715b7f4b86b877f2732f
Zain Adil <xain90@gmail.com>
2013-12-01 22:10:05 -0500

    CSS and JS Changes

--- WebContent/res/foodrus.js
+++ WebContent/res/foodrus.js
@@ -1,13 +1,22 @@
 
-function updateQuanity(itemID){
+function updateQuanity(itemID) {
 	var qty = document.getElementById(itemID).value;
-	document.getElementById("updatedIDandQty").value = itemID + ";" + qty;
+
+	if (isNaN(qty)) {
+		alert("Invalid Quantity");
+		return false;
+	} else {
+		document.getElementById("updatedIDandQty").value = itemID + ";" + qty;
+		return true;
+	}
 }
 
 function addtoCart(itemID){
 	var qty = document.getElementById(itemID).value;
 	if(isNaN(qty)){
-			alert("WTF");
+			alert("Invalid Quantity");
+			var id = "q"+itemID;
+			document.getElementById(id).style.background="red";
 			return false;
 	} else{
 		document.getElementById("addedIDandQty").value = itemID + ";" + qty;
@@ -16,7 +25,6 @@ function addtoCart(itemID){
 }
 
 // Hack, do something about this later.
-
 function closeNotificatonDiv(){
 	document.getElementById("addtoCartNotification").style.display=" none";
 }
