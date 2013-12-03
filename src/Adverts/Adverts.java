a7d4a5c1d94704c90e62633c593baa936d7f7bd6
Zain Adil <xain90@gmail.com>
2013-12-03 02:03:06 -0500

    Adverts

--- src/Adverts/Adverts.java
+++ src/Adverts/Adverts.java
@@ -1,6 +1,8 @@
 package Adverts;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.List;
 
 import javax.servlet.DispatcherType;
 import javax.servlet.Filter;
@@ -12,20 +14,24 @@ import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.FoodRus;
+import model.ItemBean;
 
 /**
  * Servlet Filter implementation class Adverts
  */
-@WebFilter(
-		dispatcherTypes = {
-				DispatcherType.REQUEST, DispatcherType.FORWARD, } , 
-		urlPatterns = { 
-				"/Adverts", 
-				"/Category/*"
-		}, 
-		servletNames = { "eFoods" })
+@WebFilter(dispatcherTypes = {
+				DispatcherType.REQUEST, 
+				DispatcherType.FORWARD
+		}
+					, urlPatterns = { "/Adverts" }, servletNames = { "Controller.eFoods" })
 public class Adverts implements Filter {
-
+	
+	private static final String recommended = "2002H712";
+	private static final String addedItem = "2910h019";
+	
     /**
      * Default constructor. 
      */
@@ -45,15 +51,26 @@ public class Adverts implements Filter {
 	 */
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		
-		System.out.println("Came into the Filter");
-		
 		HttpServletRequest request = (HttpServletRequest)req;
 		HttpServletResponse response = (HttpServletResponse)res;
 		
-		if(request.getParameter("addedIDandQty") != null)
-			System.out.println("Advertsss Nigga!");
-		else System.out.println("No Adverts");
-		
+		if(request.getParameter("addedIDandQty") != null){
+			
+			HttpSession session = request.getSession();
+			
+			String itemID = request.getParameter("addedIDandQty").split(";")[0];
+			if(itemID.compareToIgnoreCase(addedItem) == 0){
+				FoodRus model = (FoodRus)request.getServletContext().getAttribute("fru");
+				try {
+					System.out.println(recommended);
+					List<ItemBean> advertItems = model.retrieveItemsBySearch(recommended);
+					request.setAttribute("adverts",advertItems);
+					
+				} catch (Exception e) {
+					System.out.println("Error in Filter: " + e.getLocalizedMessage());
+				}
+			}
+		}
 		chain.doFilter(request, response);
 	}
 
